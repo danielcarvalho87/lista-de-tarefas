cf2deba2059afdf7e635421caaaf83a37351874a
Daniel Carvalho <daniel.a.carvalho@outlook.com>
2024-01-06 15:39:01 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { MdCheckCircle } from "react-icons/md";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const ESCAPE_KEY = 27;
+  const ENTER_KEY = 13;
+
+  const initialList = [
+    { id: 1, title: "titulo 1", checked: false },
+    { id: 2, title: "titulo 2", checked: true },
+    { id: 3, title: "titulo 3", checked: false },
+  ];
+
+  const [tasks] = useState(initialList);
+  const [value, setValue] = useState("");
+
+  const erase = () => {
+    setValue("");
+  };
+
+  const submit = () => {
+    console.log("submit", value);
+    erase();
+  };
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onKeyDown = (event) => {
+    if (event.which === ENTER_KEY) {
+      submit();
+    } else if (event.which === ESCAPE_KEY) {
+      erase();
+    }
+  };
+
   return (
-    <div className="App">
+    <>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Lista de tarefas</h1>
       </header>
-    </div>
+      <p>&nbsp;</p>
+      <section className="container">
+        <section className="main">
+          <input
+            className="NewItem"
+            placeholder="O que precisa ser feito?"
+            value={value}
+            onChange={onChange}
+            onKeyDown={onKeyDown}
+          />
+        </section>
+        <ul className="MainList">
+          {tasks.map((tasks) => (
+            <li key={tasks.id.toString()}>
+              <span className="titleList">{tasks.title}</span>
+              <button className="remove" type="button">
+                <MdCheckCircle size={40} />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
   );
-}
+};
 
 export default App;
